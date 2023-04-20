0b231ea1e54be6a8f440e54089085cb05d1633aa
Karol Krizka <kkrizka@gmail.com>
2023-04-20 09:19:08 +0100

    Add configurable repository.

--- AlmaLinux9/build.sh
+++ AlmaLinux9/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "### Building the OS-environment image" && \
-docker build -t mucoll-environment:2.0-el9 --progress=plain - < Dockerfile-environment && \
+docker build -t infnpd/mucoll-environment:2.0-el9 --progress=plain - < Dockerfile-environment && \
 #
 echo "### Building the Spack image" && \
-docker build -t mucoll-spack:2.0-el9 --progress=plain - < Dockerfile-spack && \
+docker build -t infnpd/mucoll-spack:2.0-el9 --progress=plain - < Dockerfile-spack && \
 #
 echo "### Building the MuColl simulation image" && \
-tar -ch . | docker build -t mucoll-sim:2.0-el9 --progress=plain -
+tar -ch . | docker build -t infnpd/mucoll-sim:2.0-el9 --progress=plain -
--- CentOS8/run.sh
+++ CentOS8/run.sh
@@ -4,5 +4,5 @@ HOME="/home/sim"
 
 docker run -it \
 -v ../.bash_profile:${HOME}/.bash_profile \
-mucoll-sim:2.0-cs8stream
+infnpd/mucoll-sim:2.0-cs8stream
 
