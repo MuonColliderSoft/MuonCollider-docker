e264a912b35c6c9ce8f45c225e2476a2449f0227
Nazar Bartosik <nazar.bartosik@cern.ch>
2023-05-26 18:38:24 +0200

    Made repo/version configurable in the build script

--- AlmaLinux9/build.sh
+++ AlmaLinux9/build.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
+# USAGE: ./build.sh [<version>] [<repository>]
 
+VERSION="devel"
+REPOSITORY="infnpd"
+SUFFIX="el9"
+
+if [ "$#" -gt 0 ]; then
+	VERSION=$1
+fi
+if [ "$#" -gt 1 ]; then
+	REPOSITORY=$2
+fi
+
+echo "### Building Docker images: ${REPOSITORY}/<IMAGE>:${VERSION}-${SUFFIX}"
+echo
 echo "### Building the OS-environment image" && \
-docker build -t infnpd/mucoll-environment:2.0-el9 --progress=plain - < Dockerfile-environment && \
+docker build -t ${REPOSITORY}/mucoll-environment:${VERSION}-${SUFFIX} --build-arg REPOSITORY --build-arg VERSION --progress=plain - < Dockerfile-environment && \
 #
 echo "### Building the Spack image" && \
-docker build -t infnpd/mucoll-spack:2.0-el9 --progress=plain - < Dockerfile-spack && \
+docker build -t ${REPOSITORY}/mucoll-spack:${VERSION}-${SUFFIX} --build-arg REPOSITORY --build-arg VERSION --progress=plain - < Dockerfile-spack && \
 #
 echo "### Building the MuColl simulation image" && \
-tar -ch . | docker build -t infnpd/mucoll-sim:2.0-el9 --progress=plain -
+tar -ch . | docker build -t ${REPOSITORY}/mucoll-sim:${VERSION}-${SUFFIX} --build-arg REPOSITORY --build-arg VERSION --progress=plain -
